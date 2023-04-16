c3c4a1b8f30fbcbfc08b8af3447cf6c06e4e83a9
Kael Adair <kja75810@gmail.com>
2023-04-16 14:46:36 -0500

    Add color change on mouse hover

--- js/index.js
+++ js/index.js
@@ -5,4 +5,9 @@ for (let i = 0; i < 16 * 16; i++) {
     const gridItem = document.createElement("div");
     gridItem.classList.add("grid-item");
     gridContainer.appendChild(gridItem);
+
+    gridItem.addEventListener('mouseover', () => {
+        gridItem.style.backgroundColor = 'cyan';
+    });
 }
+
